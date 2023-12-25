22e9309186ee1c15cb13814936f9eeb23685ae40
Tomas Polivka <tom.polivka96@gmail.com>
2023-12-25 08:52:56 +0100

    Improved multiprocessing and added results object

--- main.py
+++ main.py
@@ -1,31 +1,66 @@
 import json
 from concurrent.futures import ProcessPoolExecutor, as_completed
+from multiprocessing import Manager
+from multiprocessing.managers import DictProxy, ListProxy
+
 from src.algs.alg_list import alg_list
 from src.functions.functions import functions_list
 
 get_FES = lambda dimension: 2000 * dimension
-DIMENSIONS = [2, 10, 30]
+DIMENSIONS = [2,10,30]
 LOOP_SIZE = 30
-results = {}
+manager = Manager()
+results = manager.dict()
+
+def convert_manager_dict_to_dict(manager_dict):
+    # Convert manager.dict() to a regular dictionary recursively
+    result_dict = {}
+    for key, value in manager_dict.items():
+        if type(value) == DictProxy:
+            result_dict[key] = convert_manager_dict_to_dict(value)
+        elif type(value) == ListProxy:
+            result_dict[key] = convert_manager_list_to_list(value)
+        else:
+            result_dict[key] = value
+    return result_dict
+
+def convert_manager_list_to_list(manager_list, replace_infinity_with=None):
+    # Convert manager.list() to a regular list recursively
+    result_list = []
+    for item in manager_list:
+        if type(item) == ListProxy:
+            result_list.append(convert_manager_list_to_list(item, replace_infinity_with))
+        elif type(item) == DictProxy:
+            result_list.append(convert_manager_dict_to_dict(item))
+        elif item == float('inf'):
+            result_list.append(replace_infinity_with)  # Replace infinity with the specified value
+        else:
+            result_list.append(item)
+    return result_list
+
+
 
 def run_algorithm(obj_func, dimension, alg, fes):
     result = alg(obj_func, dimension, fes)
     return result
 
 def update_results(dim_name, obj_func_name, alg_name, result):
+    print(alg_name,result)
     if dim_name not in results:
-        results[dim_name] = {}
+        results[dim_name] = manager.dict()
     if obj_func_name not in results[dim_name]:
-        results[dim_name][obj_func_name] = {}
+        results[dim_name][obj_func_name] = manager.dict()
     if alg_name not in results[dim_name][obj_func_name]:
-        results[dim_name][obj_func_name][alg_name] = []
-    print("exec",dim_name, obj_func_name, alg_name, result)
+        results[dim_name][obj_func_name][alg_name] = manager.list()
+    print("exec", dim_name, obj_func_name, alg_name, result)
     results[dim_name][obj_func_name][alg_name].append(result)
 
 def save_results_to_file(filename):
+    result_as_dict = convert_manager_dict_to_dict(results)
+    print(result_as_dict)
     with open(filename, "w") as file:
-        json.dump(results, file, indent=4)
-    print(f"Results saved to {filename}")
+        json.dump(result_as_dict, file, indent=4)
+    # print(f"Results saved to {filename}")
 
 def main():
     with ProcessPoolExecutor() as executor:
@@ -56,4 +91,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
